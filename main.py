363a39f542b1b200e071fec899f67e86e1afcbb3
Artjoms <a.f.gamesyt123@gmail.com>
2026-06-20 01:06:28 +0300

    Finished the record marco part

--- main.py
+++ main.py
@@ -1,36 +1,64 @@
 from pynput import mouse, keyboard
 from datetime import datetime
+from time import time
+from os import makedirs
+
+makedirs("macros", exist_ok=True)
 
 monitoring = False
 buffer: list[str] = []
-last_command = datetime.now()
+last_command = time()
 
-def get_same_command_indexes(buffer_list: list[str], i: int):
+def get_same_command_indexes(buffer_list: list[str], i: int) -> int:
     try:
         if buffer_list[i].split(" ")[0] != buffer_list[i+1].split(" ")[0]:
-            # command's different!!!!!!
+            return i
             pass
         else:
-            # command's the same!!!!!!
+            get_same_command_indexes(buffer_list, i+1)
             pass
     except IndexError:
-        # fuck you
+        return i
         pass
 
 
 def on_move(x, y):
+    global last_command
 
     if monitoring:
+        wait_time = time()-last_command
+
+        if wait_time > 0.02:
+            buffer.append(f"WAIT {wait_time}\n")
+
+        last_command = time()
+
         buffer.append(f"MOVE {x} {y}\n")
 
 def on_click(x, y, button, pressed):
+    global last_command
 
     if monitoring:
+        wait_time = time() - last_command
+
+        if wait_time > 0.02:
+            buffer.append(f"WAIT {wait_time}\n")
+
+        last_command = time()
+
         buffer.append(f"CLICK {x} {y} {button} {pressed}\n")
 
 def on_scroll(x, y, dx, dy):
+    global last_command
 
     if monitoring:
+        wait_time = time() - last_command
+
+        if wait_time > 0.02:
+            buffer.append(f"WAIT {wait_time}\n")
+
+        last_command = time()
+
         buffer.append(f"SCROLL {x} {y} {dy}\n")
 
 
@@ -42,12 +70,20 @@ mouse_listener = mouse.Listener(
 
 
 def on_press(key):
+    global last_command
 
     if monitoring and key != keyboard.Key.page_down:
+        wait_time = time() - last_command
+
+        if wait_time > 0.02:
+            buffer.append(f"WAIT {wait_time}\n")
+
+        last_command = time()
+
         buffer.append(f"PRESS {key}\n")
 
 def on_release(key):
-    global monitoring, buffer
+    global monitoring, buffer, last_command
 
     if not monitoring and key == keyboard.Key.page_up:
         monitoring = True
@@ -64,17 +100,25 @@ def on_release(key):
     if monitoring and key == keyboard.Key.page_down:
         monitoring = False
 
+        if buffer[0].split(" ")[0] == "WAIT":
+            buffer = buffer[1:]
+
         marco_path = f"macros/{str(datetime.now()).replace(":", ".")}.bmacro"
-        print(f"Wrote macro to {marco_path}")
-
-
 
         with open(marco_path, "w") as f:
             f.writelines(buffer)
 
+        print(f"Wrote macro to {marco_path}")
         return
 
     if monitoring:
+        wait_time = time() - last_command
+
+        if wait_time > 0.02:
+            buffer.append(f"WAIT {wait_time}\n")
+
+        last_command = time()
+
         buffer.append(f"RELEASE {key}\n")
 
 
